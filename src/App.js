2c7fa8fa8336d4a3497a2c4760e971fe2f471d8b
Matt Poje <matt.poje@ibm.com>
2022-06-23 17:29:43 -0400

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RegistrationPage from './content/RegistrationPage';
+
+import { Content, Theme } from '@carbon/react';
+import TutorialHeader from './components/TutorialHeader';
+
+import './app.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello Carbon! Well, not quite yet. This is the starting point for the
-        Carbon tutorial.
-      </div>
+      <>
+        <Theme theme="g90">
+          <TutorialHeader />
+        </Theme>
+        <Content>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/registration" component={RegistrationPage} />
+          </Switch>
+        </Content>
+      </>
     );
   }
 }
---
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Header,
+  HeaderContainer,
+  HeaderName,
+  HeaderNavigation,
+  HeaderMenuButton,
+  HeaderMenuItem,
+  HeaderGlobalBar,
+  HeaderGlobalAction,
+  SkipToContent,
+  SideNav,
+  SideNavItems,
+  HeaderSideNavItems,
+} from '@carbon/react';
+import { Switcher, Notification, UserAvatar } from '@carbon/react/icons';
+
+const TutorialHeader = () => (
+  <HeaderContainer
+    render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+      <Header aria-label="Carbon Tutorial">
+        <SkipToContent />
+        <HeaderMenuButton
+          aria-label="Open menu"
+          onClick={onClickSideNavExpand}
+          isActive={isSideNavExpanded}
+        />
+        <HeaderName element={Link} to="/" prefix="IBM">
+          Carbon Landing
+        </HeaderName>
+        <HeaderNavigation aria-label="Carbon Tutorial">
+          <HeaderMenuItem element={Link} to="/registration">
+            Registration
+          </HeaderMenuItem>
+        </HeaderNavigation>
+        <SideNav
+          aria-label="Side navigation"
+          expanded={isSideNavExpanded}
+          isPersistent={false}>
+          <SideNavItems>
+            <HeaderSideNavItems>
+              <HeaderMenuItem element={Link} to="/registration">
+                Registration
+              </HeaderMenuItem>
+            </HeaderSideNavItems>
+          </SideNavItems>
+        </SideNav>
+        <HeaderGlobalBar>
+          <HeaderGlobalAction
+            aria-label="Notifications"
+            tooltipAlignment="center">
+            <Notification size={20} />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction
+            aria-label="User Avatar"
+            tooltipAlignment="center">
+            <UserAvatar size={20} />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="App Switcher" tooltipAlignment="end">
+            <Switcher size={20} />
+          </HeaderGlobalAction>
+        </HeaderGlobalBar>
+      </Header>
+    )}
+  />
+);
+
+export default TutorialHeader;
