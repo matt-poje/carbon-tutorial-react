7c7b934e41cf79f76c92a160d726774f06ed0309
Matt Poje <matt.poje@ibm.com>
2022-06-27 10:37:22 -0400

    feat(tutorial): complete step 3

--- src/content/RepoPage/RepoTable.js
+++ src/content/RepoPage/RepoTable.js
@@ -14,6 +14,10 @@ import {
 } from '@carbon/react';
 
 const RepoTable = ({ rows, headers }) => {
+  const getRowDescription = rowId => {
+    const row = rows.find(({ id }) => id === rowId);
+    return row && row?.description ? row.description : '(no description)';
+  };
   return (
     <DataTable
       rows={rows}
@@ -48,7 +52,7 @@ const RepoTable = ({ rows, headers }) => {
                     ))}
                   </TableExpandRow>
                   <TableExpandedRow colSpan={headers.length + 1}>
-                    <p>Row description</p>
+                    <small>{getRowDescription(row.id)}</small>
                   </TableExpandedRow>
                 </React.Fragment>
               ))}
